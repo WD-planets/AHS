4b7d12219a9b69a313ea4f1c7a08ed5a5519d9f1
pizquierdosanchez <99327391+pizquierdosanchez@users.noreply.github.com>
2024-01-16 11:12:36 +0000

    Add files via upload

--- ahs_forBTG.py
+++ ahs_forBTG.py
@@ -1,6 +1,7 @@
 '''
 This code has different functions in terms of how many dips you see per transit. 
-It's rudimentary. I tried to generalise it with '
+It's rudimentary. I tried to generalise it with lines 38 to 54 but failed. If you want
+to explore that option what fails is the tuple to feed to curve fit
 
 Procedure: I let the out of transit level, the depth factor scale, the
 time at minimum flux and the ingress and egress time-scales as free parameters for the first time we fit 
